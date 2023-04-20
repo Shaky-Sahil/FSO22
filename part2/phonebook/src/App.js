0320056cc25c246226fae70ebc944499fb97735b
Sahil Shaikh <sahilpk81@gmail.com>
2023-04-20 16:52:08 +0530

    ex 2.9 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,41 +2,62 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phone: '9194' },
+    { name: 'james jones', phone: '9194' },
+    { name: 'kobe', phone: '9194' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [filter, setFilter] = useState('')
+  const personsToShow = showAll? persons : persons.filter(p => p.name.toLowerCase().includes(filter))
 
   const updateName = (e) => {
     setNewName(e.target.value)
   }
 
-  const addName = (e) => {
+  const addPerson = (e) => {
     e.preventDefault()
-    const newPerson = {name:newName}
+    const newPerson = {name:newName,phone:newPhone}
     console.log(newPerson)
     checkDuplicate(newName) === true ? alert(`${newName} is a duplicate name`) : setPersons(persons.concat(newPerson))
-    
     setNewName('')
+    setNewPhone('')
+  }
+
+  const updatePhone = (e) => {
+    setNewPhone(e.target.value)
   }
 
   const checkDuplicate = (newName) => {
     const duplicates = persons.filter(p=>p.name === newName)
     return duplicates.length===0? false : true
   }
+  const createFilter = (e) => {
+    setShowAll(false)
+    setFilter(e.target.value.toLowerCase())
+    
+  }
 
+ 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with<input onChange={createFilter}/>
+      </div>
+      <h2>add a new</h2>
       <form>
         <div>
-          name: <input value={newName} onChange={updateName} />
+          name: <input value={newName} onChange={updateName} /><br/>
+          number: <input value={newPhone} onChange={updatePhone}/>
         </div>
         <div>
-          <button type="submit" onClick={addName}>add</button>
+          <button type="submit" onClick={addPerson}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((p)=><li key={p.name}>{p.name}</li>)}
+      {personsToShow.map((p)=><li key={p.name}>{p.name} {p.phone}</li>) }
     </div>
   )
 }
